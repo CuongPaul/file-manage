f85ea138b3721870ba45262eefe0a62fd3c5f2c4
Ta Cuong <cuongvt@appota.com>
2023-12-11 15:35:00 +0700

    improve(codebase): env, joi, husky, commitlint, prettier, dockerizing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { databaseConfig } from './configs/configuration.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			cache: true,
+			isGlobal: true,
+			expandVariables: true,
+			load: [databaseConfig],
+			envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
+
+			validationOptions: { abortEarly: false },
+			validationSchema: Joi.object({
+				PORT: Joi.number().default(4000),
+				NODE_ENV: Joi.string()
+					.valid('test', 'staging', 'production', 'development')
+					.default('development'),
+			}),
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const logger = new Logger(bootstrap.name);
+
+	const app = await NestFactory.create(AppModule);
+
+	const configService = app.get(ConfigService);
+
+	const port = configService.get('API_PORT');
+
+	await app.listen(port, () => logger.log(`Listening on port ${port}`));
 }
+
 bootstrap();
